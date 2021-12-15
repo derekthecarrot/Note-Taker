6fbf6e4f64548ebbcc2a3ec57428d22800a1c31d
derekthecarrot <jpastvait@gmail.com>
2021-12-15 14:48:26 -0500

    adding get, post routes, adding routes and save,delete,add functionality

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const express = require('express');
-const path = require('path');
 
-const app = express()
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const app = express();
+
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    
-});
+app.use(express.json());
 
-app.get('/notes', (req, res) => {
-    res.send(notes.html)
-});
-
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
-    console.log('App is listening at PORT 3001')
+    console.log(`App listening on ${PORT}!`);
 });
